cd4f4df7dd337bd074213f6f161f3d079d8ceb0d
MrDmitry-dev <78802250+MrDmitry-dev@users.noreply.github.com>
2022-02-17 10:08:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -162,7 +162,6 @@ function render() {
 let buttonToRotate = document.querySelector('button')
 
 buttonToRotate.addEventListener('click', () => {
-    console.log('it works');
-    scenes[0].rotation.y -= Math.PI / 2
-    scenes[1].rotation.y -= Math.PI / 2
+    scenes[0].rotation.y -= Math.PI / 2;
+    scenes[1].rotation.y -= Math.PI / 2;
 })
