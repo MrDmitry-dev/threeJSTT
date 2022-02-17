b06bbfa705b6b0bdb2e8cb36e0f3e3e2416e01de
MrDmitry-dev <dimaancudinov@gmail.com>
2022-02-17 09:21:27 +0300

    it's time to make a button work

--- main.js
+++ main.js
@@ -1,46 +1,210 @@
 import './style.css'
 import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-const loader = new GLTFLoader();
+const loader = new GLTFLoader()
+    // const scene = new THREE.Scene()
+    // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.7, 10000)
+    // const light = new THREE.AmbientLight(0xbcbcbc) // soft white light
+    // const renderer = new THREE.WebGLRenderer()
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x6baaf8);
+// // './models/squareRoom.glb'
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.7, 1000);
+// function animateAndRotateModels(pathOfmodel) {
+//     scene.background = new THREE.Color(0xF4F6F7);
+//     scene.add(light);
 
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     document.body.appendChild(renderer.domElement);
 
+//     loader.load(pathOfmodel, function(gltf) {
+//         scene.rotation.x += Math.PI / 2
+//             // scene.rotation.y += 0.3
+//         scene.add(gltf.scene);
+//         gltf.scene.scale.set(20, 20, 20);
+//     }, undefined, function(error) {
+//         console.error(error);
+//     });
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-document.body.appendChild(renderer.domElement);
+//     camera.position.z = 8
 
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF
-// });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+//     function animate() {
+//         requestAnimationFrame(animate)
+//             // scene.rotation.y += 0.01;
+//         renderer.render(scene, camera)
+//     }
 
-let room
+//     let btn = document.createElement("button")
+//     btn.innerHTML = "Rotate it"
+//     document.body.appendChild(btn)
 
-loader.load('./models/squareRoom.glb', function (gltf) {
-	scene.add(gltf.scene);
-	gltf.scene.scale.set(10, 10, 10);
-}, undefined, function (error) {
-	console.error(error);
-});
+//     btn.addEventListener('click', () => scene.rotation.y += Math.PI / 2)
 
-camera.position.z = 5;
+//     animate()
+// }
 
-function animate() {
-	requestAnimationFrame(animate);
-	// scene.children[0].scale
-	// scene.scale = {x:3,y:3,z:3}
-	// scene.children[0].rotation.x += 0.01;
-	scene.rotation.y += 0.01;
+// animateAndRotateModels('./models/squareRoom.glb')
+// animateAndRotateModels('./models/lRoom.glb')
 
-	renderer.render(scene, camera);
-};
+// import { OrbitControls } from './OrbitControls.js';
 
+let canvas, renderer;
+
+const scenes = [];
+
+init();
 animate();
 
+function init() {
+
+    canvas = document.getElementById("c");
+
+    // const geometries = [
+    //     new THREE.BoxGeometry(1, 1, 1),
+    //     new THREE.SphereGeometry(0.5, 12, 8),
+    //     new THREE.DodecahedronGeometry(0.5),
+    //     new THREE.CylinderGeometry(0.5, 0.5, 1, 12)
+    // ];
+
+    const content = document.getElementById('content');
+
+    for (let i = 0; i < 2; i++) {
+
+        const scene = new THREE.Scene();
+
+        // make a list item
+        const element = document.createElement('div');
+        element.className = 'list-item';
+
+        const sceneElement = document.createElement('div');
+        element.appendChild(sceneElement);
+
+        const descriptionElement = document.createElement('div');
+        descriptionElement.innerText = 'Scene ' + (i + 1);
+        element.appendChild(descriptionElement);
+
+        // the element that represents the area we want to render the scene
+        scene.userData.element = sceneElement;
+        content.appendChild(element);
+
+        // const camera = new THREE.PerspectiveCamera(50, 1, 1, 10);
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        camera.position.z = 10;
+        scene.userData.camera = camera;
+        scene.rotation.x += Math.PI / 2
+
+        // const controls = new OrbitControls(scene.userData.camera, scene.userData.element);
+        // controls.minDistance = 2;
+        // controls.maxDistance = 5;
+        // controls.enablePan = false;
+        // controls.enableZoom = false;
+        // scene.userData.controls = controls;
+
+        if (i == 0) {
+            loader.load('./models/squareRoom.glb', function(gltf) {
+                // scene.rotation.x += Math.PI / 2
+                // scene.rotation.y += 0.3
+                scene.add(gltf.scene);
+                gltf.scene.scale.set(20, 20, 20);
+            }, undefined, function(error) {
+                console.error(error);
+            });
+        } else {
+            loader.load('./models/lRoom.glb', function(gltf) {
+                // scene.rotation.x += Math.PI / 2
+                // scene.rotation.y += 0.3
+                scene.add(gltf.scene);
+                gltf.scene.scale.set(20, 20, 20);
+            }, undefined, function(error) {
+                console.error(error);
+            });
+        }
+
+        scene.add(new THREE.HemisphereLight(0xaaaaaa, 0x444444));
+
+        const light = new THREE.DirectionalLight(0xffffff, 0.5);
+        light.position.set(1, 1, 1);
+        scene.add(light);
+
+        scenes.push(scene);
+    }
+
+    renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+    renderer.setClearColor(0xffffff, 1);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+}
+
+function updateSize() {
+
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+
+    if (canvas.width !== width || canvas.height !== height) {
+
+        renderer.setSize(width, height, false);
+
+    }
+
+}
+
+function animate() {
+
+    render();
+    requestAnimationFrame(animate);
+
+}
+
+function render() {
+
+    updateSize();
+
+    canvas.style.transform = `translateY(${window.scrollY}px)`;
+
+    renderer.setClearColor(0xffffff);
+    renderer.setScissorTest(false);
+    renderer.clear();
+
+    renderer.setClearColor(0xe0e0e0);
+    renderer.setScissorTest(true);
+
+    scenes.forEach(function(scene) {
+
+        // so something moves
+        // MOVING IS HERE
+        // scene.children[0].rotation.y = Date.now() * 0.001;
+
+        // get the element that is a place holder for where we want to
+        // draw the scene
+        const element = scene.userData.element;
+
+        // get its position relative to the page's viewport
+        const rect = element.getBoundingClientRect();
+
+        // check if it's offscreen. If so skip it
+        if (rect.bottom < 0 || rect.top > renderer.domElement.clientHeight ||
+            rect.right < 0 || rect.left > renderer.domElement.clientWidth) {
+
+            return; // it's off screen
+
+        }
+
+        // set the viewport
+        const width = rect.right - rect.left;
+        const height = rect.bottom - rect.top;
+        const left = rect.left;
+        const bottom = renderer.domElement.clientHeight - rect.bottom;
+
+        renderer.setViewport(left, bottom, width, height);
+        renderer.setScissor(left, bottom, width, height);
+
+        const camera = scene.userData.camera;
+
+        renderer.render(scene, camera);
+
+    });
+
+}
+
+let buttonToRotate = document.querySelector('button')
